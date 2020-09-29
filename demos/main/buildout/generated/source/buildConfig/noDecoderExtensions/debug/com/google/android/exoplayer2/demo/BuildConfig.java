34b73b1517c3b8e3d4219cd77f0b5062284ec0be
bcchaudhary <bharartchaudhary20893@gmail.com>
2020-09-29 15:31:35 +0530

    init

---
+++ demos/main/buildout/generated/source/buildConfig/noDecoderExtensions/debug/com/google/android/exoplayer2/demo/BuildConfig.java
@@ -0,0 +1,15 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.google.android.exoplayer2.demo;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.google.android.exoplayer2.demo";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "noDecoderExtensions";
+  public static final int VERSION_CODE = 2012000;
+  public static final String VERSION_NAME = "2.12.0";
+  // Fields from product flavor: noDecoderExtensions
+  public static final boolean USE_DECODER_EXTENSIONS = false;
+}
---
+++ demos/surface/buildout/generated/source/buildConfig/debug/com/google/android/exoplayer2/surfacedemo/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.google.android.exoplayer2.surfacedemo;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.google.android.exoplayer2.surfacedemo";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 2012000;
+  public static final String VERSION_NAME = "2.12.0";
+}
